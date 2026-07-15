e2744239248df34a552d50bb0ce05a35125075dc
miftahrachmatullah <mochamadmiftah34@gmail.com>
2026-07-15 21:20:34 +0700

    feat: enhance database connection and add user model
    - Implemented database connection utility with DNS server configuration.
    - Added environment variable support for database URL.
    - Refactored server initialization to handle database connection asynchronously.
    - Introduced user model file for future user-related functionalities.
    - Updated package dependencies for mongoose and dotenv.

---
+++ backend/src/utils/database.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import dns from "node:dns";
+
+import { DATABASE_URL } from "./env";
+
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
+
+const connect = async () => {
+  try {
+    await mongoose.connect(DATABASE_URL, {
+      dbName: "db_acara",
+    });
+
+    return "Database connected";
+  } catch (error) {
+    throw error;
+  }
+};
+
+export default connect;
