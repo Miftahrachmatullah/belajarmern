a0421577aa5581fd7b5218133f26e3eeb6141e36
miftahrachmatullah <mochamadmiftah34@gmail.com>
2026-08-23 18:14:05 +0700

    feat: implement user login functionality with password validation

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import * as yup from "yup";
 
 import UserModel from "../models/user.model";
+import { encrypt } from "../utils/encryption";
 
 type TRegister = {
   fullName: string;
@@ -37,7 +38,6 @@ export default {
         password,
         confirmPassword,
       } = req.body as TRegister;
-
       await registerValidateSchema.validate({
         fullName,
         username,
@@ -45,25 +45,72 @@ export default {
         password,
         confirmPassword,
       });
-
       const result = await UserModel.create({
         fullName,
         username,
         email,
         password,
       })
-
       res.status(200).json({
         message: "Register successful",
         data: result,
       });
     } catch (error) {
       const err = error as Error;
-
       res.status(400).json({
         message: err.message,
         data: null,
       });
     }
   },
+  async login(req: Request, res: Response) {
+    const {
+      identifier,
+      password
+    } = req.body as unknown as TLogin;
+    try {
+      // ambil data user berdasarkan "identifier" --> username dan email 
+
+      const userByIdentifier = await UserModel.findOne({
+        $or: [
+          {
+            email: identifier
+          },
+          {
+            username: identifier,
+          },
+        ],
+      });
+
+      if(!userByIdentifier) {
+        return res.status(403).json({
+          message: "user not found",
+          data: null
+      });
+      }
+
+      // validasi password 
+      const validatePassword: boolean = 
+        encrypt(password) === userByIdentifier.password;
+
+      if(!validatePassword) {
+        return res.status(403).json({
+          message: "invalid password",
+          data: null
+      });
+      }
+
+      res.status(200).json({
+        message: "login successful",
+        data: userByIdentifier,
+      });
+
+    } catch (error) {
+      const err = error as Error;
+      res.status(400).json({
+        message: err.message,
+        data: null,
+      });
+    }
+  }
 };
